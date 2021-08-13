1c6cd389cf7fe7550997f1eb1bcafc8a0b4f1979
VenerAndrei <andrei.vener@stud.acs.upb.ro>
2021-08-13 19:05:07 +0300

    Grid functions

--- env/PacManMaze.py
+++ env/PacManMaze.py
@@ -2,27 +2,81 @@ import pygame
 
 pygame.init()
 done = False
-#COLORS
-WHITE = (255,255,255)
-GRAY = (255/2,255/2,255/2)
+# MAP
+
+# COLORS
+WHITE = (255, 255, 255)
+GRAY = (255 // 2, 255 // 2, 255 // 2)
 BLACK = (0, 0, 0)
-#SCREEN SIZES
-tile_size = 16;
+# SCREEN SIZES
+tile_size = 16
 width = 28 * tile_size
 height = 31 * tile_size
 screen = pygame.display.set_mode((width, height))
-
+cols, rows = (28, 31)
+grid = [[0 for i in range(cols)] for j in range(rows)]
+print(grid)
 sprites = pygame.image.load("../images/map.png")
+
+
+def drawLine(at, to, grid):
+    print(to, at)
+    if at[0] == to[0]:
+        decY = 1
+        if at[1] - to[1] > 0:
+            for x in range(to[1], at[1] + 1):
+                grid[x][at[0]] = 1
+            return
+        for x in range(at[1], to[1]+1):
+            grid[x][at[0]] = 1
+        return
+    elif at[1] == to[1]:
+
+        if at[0] - to[0] > 0:
+            for x in range(to[0], at[0] + 1):
+                grid[at[1]][x] = 1
+            return
+
+        for x in range(at[0], to[0]+1):
+            grid[at[1]][x] = 1
+        return
+
+
+mouseCounter = 0
+mouseRes = [0, 0]
+at = 0
+to = 0
 while not done:
     screen.fill(BLACK)
-    screen.blit(sprites,(0,0),(0,tile_size*3,width,height))
+    screen.blit(sprites, (0, 0), (0, tile_size * 3, width, height))
 
-    for k in range(0,28):
-        pygame.draw.line(screen,GRAY,(k*tile_size,0),(k*tile_size,height))
+    for k in range(0, 28):
+        pygame.draw.line(screen, GRAY, (k * tile_size, 0), (k * tile_size, height))
     for k in range(0, 31):
-        pygame.draw.line(screen, GRAY, (0,k*tile_size), (width,k*tile_size))
+        pygame.draw.line(screen, GRAY, (0, k * tile_size), (width, k * tile_size))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if (mouseCounter%2 == 0):
+                res = pygame.mouse.get_pos()
+                at = res
+                print("Pressed at {} , {}\t that is at {},{}\n".format(res[0], res[1], res[0] // tile_size,
+                                                                       res[1] // tile_size))
+            if (mouseCounter%2 == 1):
+                res = pygame.mouse.get_pos()
+                to = res
+                print("Pressed at {} , {}\t that is at {},{}\n".format(res[0], res[1], res[0] // tile_size,
+                                                                       res[1] // tile_size))
+                drawLine([at[0] // tile_size, at[1] // tile_size],
+                         [to[0] // tile_size, to[1] // tile_size],
+                         grid)
+                print("Line Draw")
+            mouseCounter += 1
+
+    # print(pygame.mouse.get_pressed(1))
 
     pygame.display.flip()
+
+for x in grid:
+    print(x)
